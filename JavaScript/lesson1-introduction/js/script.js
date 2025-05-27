8939938eb825af08a32ec21619326e4afd33e105
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-05-27 20:19:43 +0400

    js start

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,36 @@
+//one line
+/**
+ * more 
+ * line
+ */
+//   console.log("Hello JavaScript");
+
+
+
+// var x = 5;
+// console.log(x);
+
+
+// Data Types
+// Primitive DT 
+var infoStr  = "Hello"; //string
+var infoNumb = 5; //number
+var infoBoo = true; //boolean
+
+// Composite DT
+var infoArr = ['Hello',5,true];
+var infoObj = {
+    brand:"BMW",
+    price:30000,
+    newCar:true
+}
+
+// Special DT
+var infoUnd = undefined;
+var infoNul = null;
+
+var info = null;
+console.log(info);
+
+
+
