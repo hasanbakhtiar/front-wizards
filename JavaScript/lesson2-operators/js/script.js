4127513a0f31ab990b53b8fb5f06a34a61ca9779
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-05-27 21:07:50 +0400

    js start

---
+++ JavaScript/lesson2-operators/js/script.js
@@ -0,0 +1,44 @@
+
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+// var a = 5;
+// a **= 10;
+// console.log(a);
+
+
+// a **=10; // a = a + 10;
+// console.log(a);
+
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// var a = 5;
+// a++;
+// console.log(a);
+
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+
