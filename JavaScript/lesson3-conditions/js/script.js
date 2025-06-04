b1bb2cac68e8c9425f0148b474eb2074fa79baca
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-06-04 20:59:50 +0400

    loops

--- JavaScript/lesson3-conditions/js/script.js
+++ JavaScript/lesson3-conditions/js/script.js
@@ -1,22 +1,75 @@
-// if = eger 
+
+// try {
+//     throw("no problem");
+//     consolie.log("Hello");
+// } catch (err) {
+//    console.log(err) 
+// }finally{
+//     console.log('im working everytime')
+// }
+//
+
+
+
+
+
+
+
+
+// let info = 7;
+//
+// switch (info) {
+//     case 5:
+//     case 6:
+//     case 7:
+//     case 8:
+//     case 9:
+//         console.log('This is 5');
+//         break;
+//     case 10:
+//         console.log('this is 10');
+//         info+=20;
+//         if (info == 30) {
+//             console.log("Add 20");
+//         }
+//         break;
+//     case 'Hello':
+//         console.log('This is Hello');
+//         break;
+//     default:
+//         console.log('This is default');
+//         break;
+// }
+
+// let payment = true;
+// let status = '';
+//
+// if (payment) {
+//     status = "Success";
+// } else {
+//     status = "Error";
+// }
+// console.log(status)
+
+// if = eger
 // else = deyilse
-// else if  = deyilse eger 
+// else if  = deyilse eger
 
-const time = 23;
-
-if (time >=5 && time < 12) {
-    console.log('good morning');
-    
-}else if(time >=12 && time <= 17){
-    console.log('good afternoon');
-    
-}else if(time >=18 && time <= 22){
-    console.log('good evening');
-    
-}else if(time >=23  && time <= 24){
-    console.log('good night');
-    
-}else{
-    console.log('wrong time');
-    
-}
+// const time = 9;
+//
+// if (time >= 5 && time < 12) {
+//     console.log('good morning');
+//     if (time === 7) {
+//         console.log('get up');
+//     } else if (time === 9) {
+//         console.log('have a breakfast');
+//     }
+// } else if (time >= 12 && time <= 17) {
+//     console.log('good afternoon');
+// } else if (time >= 18 && time <= 22) {
+//     console.log('good evening');
+// } else if (time >= 23 && time <= 24) {
+//     console.log('good night');
+// } else {
+//     console.log('wrong time');
+// }
---
+++ JavaScript/lesson3-conditions/lesson4-loops/js/script.js
@@ -0,0 +1,23 @@
+
+for(let info = 1; info<10; info++){
+  console.log(info)
+}
+
+
+
+// let count = 1;
+//
+// while(count>=10){
+//   console.log("Hello"+count);
+//   count++;
+// }
+
+
+// let count = 5;
+// do{
+// console.log(count);
+//   count++;
+// }while(count>10)
+//
+
+
