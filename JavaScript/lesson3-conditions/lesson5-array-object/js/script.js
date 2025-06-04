e43e05b8203b5ff656cd6b891d7e4a84a40cf85d
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-06-04 20:59:23 +0400

    loops

---
+++ JavaScript/lesson3-conditions/lesson5-array-object/js/script.js
@@ -0,0 +1,10 @@
+const fruits = ['Apple', 'Banana', 'Orange',
+'Grape', 'Pear'];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+// console.log(info.length);
+// console.log(info[0]);
+// console.log(info[1]);
+// console.log(info[2]);
