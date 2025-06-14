f83ef842605681a6889ff2e1b326db108860d266
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-06-14 15:15:09 +0400

    lastest

---
+++ JavaScript/lesson8-methods-part2/js/data.js
@@ -0,0 +1,43 @@
+const phones = [
+       {
+              title: 'Samsung Galaxy S24',
+              price: 1199,
+              stock: 8,
+              color: 'Phantom Silver',
+       },
+       {
+              title: 'Xiaomi 14 Ultra',
+              price: 999,
+              stock: 15,
+              color: 'Ceramic White',
+       },
+       { title: 'OnePlus 12', price: 899, stock: 5, color: 'Emerald Green' },
+       {
+              title: 'Google Pixel 8 Pro',
+              price: 1099,
+              stock: 10,
+              color: 'Obsidian',
+       },
+       { title: 'Huawei P60 Pro', price: 949, stock: 7, color: 'Rococo Pearl' },
+       { title: 'Realme GT 6', price: 649, stock: 20, color: 'Mirage Silver' },
+       {
+              title: 'Sony Xperia 1 V',
+              price: 1190,
+              stock: 4,
+              color: 'Khaki Green',
+       },
+       {
+              title: 'Motorola Edge 50 Pro',
+              price: 720,
+              stock: 9,
+              color: 'Luxe Lavender',
+       },
+       {
+              title: 'Asus ROG Phone 8',
+              price: 1350,
+              stock: 6,
+              color: 'Storm Grey',
+       },
+];
+
+export { phones };
---
+++ JavaScript/lesson8-methods-part2/js/script.js
@@ -0,0 +1,31 @@
+// "use strict";
+// import * as data from './data.js';
+// a = 5;
+// console.log(a)
+
+//
+// data.phones.forEach((item,index)=>(
+// console.log(item,index)
+// ));
+//
+
+debugger;
+console.log("test1")
+console.log("test2")
+console.log("test3")
+console.loag("test4")
+console.log("test5")
+console.log("test6")
+console.log("test7")
+
+
+
+
+// const deleteData = ()=>{
+//   return  data.phones.pop();
+// }
+//
+// deleteData();
+// deleteData();
+// deleteData();
+// console.log(data.phones);
