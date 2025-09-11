d7f580d0cc9c2abf1f7b4f50779d5a91d61a317b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-09-11 11:02:54 +0400

    lastest

---
+++ ReactJS/lesson11-sass/src/App.jsx
@@ -0,0 +1,36 @@
+const App = () => {
+  return (
+    <div className="con">
+      <ul className="box">
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>
+          <ol>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+            <li>List Item</li>
+          </ol>
+        </li>
+      </ul>
+      <ol className="second">
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+        <li>List Item</li>
+      </ol>
+    </div>
+  );
+};
+
+export default App;
